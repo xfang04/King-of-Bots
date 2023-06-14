3c21451d5b078959d175f2b993d33d14640c0f56
Xin Fang <fangxin1996@gmail.com>
2023-06-14 11:32:13 -0400

    update register view

--- web/src/views/user/account/UserAccountRegisterView.vue
+++ web/src/views/user/account/UserAccountRegisterView.vue
@@ -59,7 +59,7 @@ export default {
 
     const register = () => {
       $.ajax({
-        url: "https://app2703.acapp.acwing.com.cn/api/user/account/register/",
+        url: "http://127.0.0.1:3000/user/account/register/",
         type: "post",
         data: {
           username: username.value,
