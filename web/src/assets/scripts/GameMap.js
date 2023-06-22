b2995942dcf8651afe1a18ff340af9c40a519bb1
Xin Fang <fangxin1996@gmail.com>
2023-06-21 23:12:30 -0400

    start matching system

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,11 +3,12 @@ import { Wall } from "./Wall";
 import { Snake } from "./Snake";
 
 export class GameMap extends AcGameObject {
-  constructor(ctx, parent) {
+  constructor(ctx, parent, store) {
     super();
 
     this.ctx = ctx;
     this.parent = parent;
+    this.store = store;
     // L: 每个小正方形的边长，由浏览器窗口大小决定
     this.L = 0;
     // 当行宽相等时，可能出现两条蛇同一时间进入同一格子的情况
@@ -19,90 +20,13 @@ export class GameMap extends AcGameObject {
     this.walls = [];
 
     this.snakes = [
-      new Snake(
-        {
-          id: 0,
-          color: "#4876EC",
-          r: this.rows - 2,
-          c: 1,
-        },
-        this
-      ),
-      new Snake(
-        {
-          id: 1,
-          color: "#F94848",
-          r: 1,
-          c: this.cols - 2,
-        },
-        this
-      ),
+      new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
+      new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
     ];
   }
 
-  check_connectivity(g, sx, sy, tx, ty) {
-    // flood fill算法，检查从(sx, sy)到(tx, ty)是否连通
-    // 如果走到了(tx, ty)，返回true
-    if (sx == tx && sy == ty) return true;
-    // 将当前(sx, sy)标记为已经走过
-    g[sx][sy] = true;
-    // 定义四个方向的偏移量
-    let dx = [-1, 0, 1, 0],
-      dy = [0, 1, 0, -1];
-    // 依次尝试四个方向
-    for (let i = 0; i < 4; i++) {
-      let x = sx + dx[i],
-        y = sy + dy[i];
-      // 如果(x, y)不是障碍物，且没有走过，递归调用
-      // 如果走到了(tx, ty)，返回true
-      if (!g[x][y] && this.check_connectivity(g, x, y, tx, ty)) return true;
-    }
-
-    return false;
-  }
-
   create_walls() {
-    // 创建一个二维布尔数组，表示是否有障碍物，初始值为false
-    const g = [];
-    for (let r = 0; r < this.rows; r++) {
-      g[r] = [];
-      for (let c = 0; c < this.cols; c++) {
-        g[r][c] = false;
-      }
-    }
-
-    // 给四周加上障碍物
-    for (let r = 0; r < this.rows; r++) {
-      g[r][0] = g[r][this.cols - 1] = true;
-    }
-
-    for (let c = 0; c < this.cols; c++) {
-      g[0][c] = g[this.rows - 1][c] = true;
-    }
-
-    // 创建随机障碍物
-    // 为了公平，设置整张地图为对称的，随机生成一半的障碍物
-    // 当地图不为正方形时，无法做到轴对称，只能做到中心对称
-    for (let i = 0; i < this.inner_walls_count / 2; i++) {
-      // 如果随机生成的位置已经有障碍物了，就重新生成
-      for (let j = 0; j < 1000; j++) {
-        let r = parseInt(Math.random() * this.rows);
-        let c = parseInt(Math.random() * this.cols);
-        if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
-        // 如果随机生成的位置是左下角或者右上角，就重新生成
-        if ((r == this.rows - 2 && c == 1) || (r == 1 && c == this.cols - 2))
-          continue;
-
-        g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
-        break;
-      }
-    }
-
-    // 检查是否连通。如果不连通，返回false，重新生成障碍物
-    // 避免污染原布尔数组，复制一份g，然后检查
-    const copy_g = JSON.parse(JSON.stringify(g));
-    if (!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2))
-      return false;
+    const g = this.store.state.pk.gamemap;
 
     // 根据二维布尔数组，把障碍物加入到this.walls数组中
     for (let r = 0; r < this.rows; r++) {
@@ -112,8 +36,6 @@ export class GameMap extends AcGameObject {
         }
       }
     }
-
-    return true;
   }
 
   add_listening_events() {
@@ -133,7 +55,8 @@ export class GameMap extends AcGameObject {
   }
 
   start() {
-    for (let i = 0; i < 1000; i++) if (this.create_walls()) break;
+    this.create_walls();
+
     this.add_listening_events();
   }
 
